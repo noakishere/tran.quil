535c652b8b81b6feb357fb3079f82ba493f31c00
dlaythanb <74222647+dlaythanb@users.noreply.github.com>
2020-11-27 11:53:06 -0500

    Added Basic Functionality to Login Page

--- TranQuil/app/src/main/java/com/example/tranquil/MainActivity.java
+++ TranQuil/app/src/main/java/com/example/tranquil/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.tranquil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username, password;
-    String user, pass;
+    EditText usernameEditText, passwordEditText;
+    Button loginBtn, registerNewUserBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,28 +22,79 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // hide title bar
         setContentView(R.layout.activity_main);
 
-        // load directly into Main Menu (until login/registration page has been implemented
-        Intent intent = new Intent(MainActivity.this, MainMenu.class);
+        // declare views
+        usernameEditText = (EditText)findViewById(R.id.usernameEditTextLoginPage);
+        passwordEditText = (EditText)findViewById(R.id.passwordTextFieldLoginPage);
+        loginBtn = (Button)findViewById(R.id.loginButtonLoginPage);
+        registerNewUserBtn = (Button)findViewById(R.id.registerNewUserButtonLoginPage);
+
+        // register click event for loginBtn
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginAsExistingUser();
+            }
+        });
+
+        // register click event of registerNewUserBtn
+        registerNewUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerAsNewUser();
+            }
+        });
+    }
+
+    // validates user information, and logs in user to the main screen (main menu page)
+    private void loginAsExistingUser(){
+        // collect username and password from text fields
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // validate username and password in database
+        // username or password is left blank
+        if(username.equals("") || password.equals("")){
+            Toast.makeText(getApplicationContext(), "No fields can be left empty!", Toast.LENGTH_SHORT).show();
+            usernameEditText.setError("Cannot be left blank.");
+            passwordEditText.setError("Cannot be left blank.");
+        }
+        // username and password are correct
+        else if(validateUserCredentials() == 0){
+            Toast.makeText(getApplicationContext(), "User login is currently not supported...", Toast.LENGTH_LONG).show(); // remove once database is working
+            Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+            intent.putExtra("username", username);
+            startActivity(intent);
+        }
+        // username and password is incorrect
+        else if(validateUserCredentials() == 1){
+            Toast.makeText(getApplicationContext(), "Invalid username and password!", Toast.LENGTH_LONG).show();
+            usernameEditText.setError("Username does not exist.");
+            passwordEditText.setError("Invalid password.");
+        }
+        // username is correct and password is incorrect
+        else if(validateUserCredentials() == 2){
+            Toast.makeText(getApplicationContext(), "Invalid password!", Toast.LENGTH_SHORT).show();
+            passwordEditText.setError("Invalid password.");
+        }
+        // username is incorrect and password is correct
+        else{
+            Toast.makeText(getApplicationContext(), "Invalid username!", Toast.LENGTH_SHORT).show();
+            usernameEditText.setError("Username does not exist.");
+        }
+    }
+
+    // opens the Register page to create a new account
+    private void registerAsNewUser(){
+        Intent intent = new Intent(getApplicationContext(), Register.class);
         startActivity(intent);
     }
 
-
-//    public void openRegisterActivity(View view) {
-//        Intent intent = new Intent(MainActivity.this, Register.class);
-//        startActivity(intent);
-//    }
-
-//    public void loginAndValidate(View view) {
-//
-//        // validation goes here
-//        username = (EditText)findViewById(R.id.idTextView);
-//        password = (EditText)findViewById(R.id.passwordTextView);
-//        user = username.getText().toString();
-//        pass = password.getText().toString();
-//        Toast.makeText(getApplicationContext(),"This is a test for the username and password ! "+"Username: " +user + " Password:  "+pass,Toast.LENGTH_LONG).show();
-//
-//        // open the main menu for now
-//       /* Intent intent = new Intent(MainActivity.this, MainMenu.class);
-//        startActivity(intent);*/
-//    }
+    // validates username and password in the database (to be supported)
+    private int validateUserCredentials(){
+        // return 0 if username and password are correct
+        // return 1 if username and password are incorrect
+        // return 2 if username is correct and password is incorrect
+        // return 3 if username is incorrect and password is correct
+        return 0;
+    }
 }
