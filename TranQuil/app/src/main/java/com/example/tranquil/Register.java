834cd97e735414e6f55143f534c6daf94ec2db89
dlaythanb <74222647+dlaythanb@users.noreply.github.com>
2020-11-27 23:53:48 -0500

    Updates to Register layout + class

--- TranQuil/app/src/main/java/com/example/tranquil/Register.java
+++ TranQuil/app/src/main/java/com/example/tranquil/Register.java
@@ -15,5 +15,6 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 }
