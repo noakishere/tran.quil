62bb8853ada5fb7e898091501f2994eae5842912
Kamyar <kamyarkarimi98@gmail.com>
2020-12-14 12:34:31 -0500

    quotes feature is implemented

--- TranQuil/app/src/main/java/com/example/tranquil/MainMenu.java
+++ TranQuil/app/src/main/java/com/example/tranquil/MainMenu.java
@@ -14,11 +14,26 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
 
 public class MainMenu extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     ImageButton myProfileBtn;
+
+    TextView randomQuote, quoteAuthor;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,10 @@ public class MainMenu extends AppCompatActivity {
 
         // declaring ImageButtons
         myProfileBtn = (ImageButton)findViewById(R.id.myProfileBtnMainMenu);
+
+        randomQuote = findViewById(R.id.dailyQuoteTextViewMainMenu);
+        quoteAuthor = findViewById(R.id.dailyQuoteAuthorTextViewMainMenu);
+        mQueue = Volley.newRequestQueue(this);
 
         // registering click events for top navigation
         myProfileBtn.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +82,40 @@ public class MainMenu extends AppCompatActivity {
                 return false;
             }
         });
+
+        jsonParse();
     }
 
+    private void jsonParse(){
+        String url = "https://www.json-generator.com/api/json/get/bPLHGLhPzC?indent=2";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("Quotes");
+
+                            Random rand = new Random();
+                            int index = rand.nextInt((jsonArray.length() - 1) + 1) + 1;
+
+                            JSONObject jsonQuote = jsonArray.getJSONObject(index);
+                            String jsonStr = jsonQuote.getString("quote");
+                            String jsonAuth = jsonQuote.getString("quoteAuthor");
+
+                            randomQuote.setText(jsonStr);
+                            quoteAuthor.setText(jsonAuth);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
 }
