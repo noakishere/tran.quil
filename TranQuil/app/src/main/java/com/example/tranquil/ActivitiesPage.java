5969224500d96ab878050dc2d53fb2a21f19a6c7
dlaythanb <74222647+dlaythanb@users.noreply.github.com>
2020-12-14 11:59:46 -0500

    Added ActivitiesPage

---
+++ TranQuil/app/src/main/java/com/example/tranquil/ActivitiesPage.java
@@ -0,0 +1,17 @@
+package com.example.tranquil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class ActivitiesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_page);
+
+    }
+}
